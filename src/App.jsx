1e9b8a7ddc2a4829e327f2c448f67c31a033c2dd
Meshach Nsude <nsudeme777@gmail.com>
2025-04-24 22:41:38 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import NoIconBtn from './components/buttons/NoIconBtn'
+
+function App() {
+
+  return (
+    <div>
+      <NoIconBtn />
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/icons/EmailIcon.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const EmailIcon = () => {
+  return (
+    <svg
+      width="13"
+      height="9"
+      viewBox="0 0 13 9"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_29_50)">
+        <path
+          d="M10.1553 0.717896H4.27209C3.03431 0.717896 2.03088 1.53753 2.03088 2.54859V6.66767C2.03088 7.67873 3.03431 8.49836 4.27209 8.49836H10.1553C11.3931 8.49836 12.3965 7.67873 12.3965 6.66767V2.54859C12.3965 1.53753 11.3931 0.717896 10.1553 0.717896Z"
+          stroke="black"
+        />
+        <path
+          d="M2.05762 2.47314L6.0946 4.36334C6.4329 4.52369 6.81707 4.60814 7.2082 4.60814C7.59933 4.60814 7.9835 4.52369 8.3218 4.36334L12.37 2.47314"
+          stroke="black"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_29_50">
+          <rect
+            width="12"
+            height="9"
+            fill="white"
+            transform="translate(0.927246)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default EmailIcon;
