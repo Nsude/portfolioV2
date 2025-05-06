fef051edde2dd5f24083a654c4d46b9f802ee80d
Meshach Nsude <nsudeme777@gmail.com>
2025-05-06 22:55:08 +0100

    update marquee animation to use animationTimeFrame

--- src/components/landing-page/Footer.jsx
+++ src/components/landing-page/Footer.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import { useNavbarContext } from '../contexts/NavbarContext';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
+  const {socials} = useNavbarContext();
+
   return (
-    <footer className='h-screen w-full' >
-      Footer
+    <footer className='h-[50vh] w-full px-desktop-h flex flex-col justify-between pb-[50px]' >
+      <div className='flex justify-between'>
+        <div className='text-45-title text-60-title flex flex-wrap w-[78%] md:w-[50%] xl:w-[40%] 2xl:w-[30%]'>
+          <span> Let's Build </span>
+          <span className='w-[90px] h-[50px] bg-myBlack hidden lg:inline-block rounded-4xl ml-[10px]'>  </span>
+          <div className='flex items-center gap-x-[10px] lg:inline-block'>
+            <span className='w-[90px] h-[50px] bg-myBlack inline-block lg:hidden rounded-4xl'>  </span>
+            <span> Something </span>
+          </div>
+          <span> Unreal </span>
+        </div>
+
+        <div className='basis-[30%]' >
+          <div className='flex justify-between mb-[35px] text-14-body opacity-40'>
+            <span>Socials</span>
+            <span>/04</span>
+          </div>
+          <div className='flex justify-between text-16-body'>
+            {
+              socials.map((item, i) => (
+                <Link key={i} to={item.link} target='_blank'>{item.title}</Link>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+
+      <div className='w-full flex justify-between'>
+        <span>{new Date().getFullYear()}</span>
+        <span>All Rights Reserved &copy;</span>
+      </div>
+
     </footer>
   )
 }
--- src/components/landing-page/Marquee.jsx
+++ src/components/landing-page/Marquee.jsx
@@ -1,75 +1,46 @@
 import { useGSAP } from "@gsap/react";
 import React, { useRef} from "react";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
 
 const Marquee = ({ children }) => {
   const containerRef = useRef();
   const marqueeRef = useRef();
   const velocitySliderRef = useRef();
+  let direction = -1;
+  let xPercent = 0;
 
   useGSAP(() => {
       if (window.matchMedia("prefers-reduced-motion: reduce").matches) return;
       const con = containerRef.current;
-      const marquee = marqueeRef.current;
-      let currentDirection = 1;
-      const velocitySlider = velocitySliderRef.current;
-      let animationInstance = null;
-
-      if (!(marquee && con && velocitySlider)) return;
-
-      const createAnimation = (direction) => {
-        if (animationInstance) {
-          animationInstance.kill();
-        }
-
-        const currentPosition = gsap.getProperty(marquee, "xPercent");
-        let endPosition = direction > 0 ? currentPosition - 33.33 : currentPosition + 33.33;
-        
-        animationInstance = gsap.fromTo(marquee, 
-          {xPercent: currentPosition},
-          {
-          xPercent: endPosition,
-          duration: 18,
-          ease: "none",
-          repeat: -1
-        })
-
-        currentDirection = direction;
-
-        return animationInstance;
-      }
-
-      createAnimation(1);
-
-      // direction control
-      ScrollTrigger.create({
-        trigger: con, 
-        start: "top bottom",
-        end: "bottom top",
-        scrub: true,
-        onUpdate: (self) => {
-          if (self.direction !== currentDirection) {
-            createAnimation(self.direction);
-          }
-        }
-      })
-
-      // velocity control
-      gsap.to(velocitySlider, {
-        x: "-=150px",
+      
+      gsap.to(velocitySliderRef.current, {
+        x: "-300px",
         scrollTrigger: {
           trigger: con,
           start: "top bottom",
           end: "bottom top",
-          scrub: true,
+          scrub: 0.25,
+          onUpdate: (self) => direction = self.direction * -1
         }
       })
       
+      
+      requestAnimationFrame(animation);
 
     },
     { scope: containerRef }
   );
+
+  const animation = () => {
+    if (xPercent <= -33.33 || (direction > 0 &&  xPercent >= 33.33)) {
+      xPercent = 0;
+    } 
+
+    gsap.set(marqueeRef.current, {xPercent});
+    requestAnimationFrame(animation);
+
+    xPercent += 0.02 * direction;
+  }
 
   return (
     <div ref={containerRef} className="overflow-hidden flex justify-center pointer-events-none">
