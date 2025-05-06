2c9851e88711ab0bd922f5861869a3e0d95d8aae
Meshach Nsude <nsudeme777@gmail.com>
2025-05-06 22:55:41 +0100

    prettier

--- src/components/landing-page/Marquee.jsx
+++ src/components/landing-page/Marquee.jsx
@@ -1,5 +1,5 @@
 import { useGSAP } from "@gsap/react";
-import React, { useRef} from "react";
+import React, { useRef } from "react";
 import gsap from "gsap";
 
 const Marquee = ({ children }) => {
@@ -9,10 +9,11 @@ const Marquee = ({ children }) => {
   let direction = -1;
   let xPercent = 0;
 
-  useGSAP(() => {
+  useGSAP(
+    () => {
       if (window.matchMedia("prefers-reduced-motion: reduce").matches) return;
       const con = containerRef.current;
-      
+
       gsap.to(velocitySliderRef.current, {
         x: "-300px",
         scrollTrigger: {
@@ -20,30 +21,31 @@ const Marquee = ({ children }) => {
           start: "top bottom",
           end: "bottom top",
           scrub: 0.25,
-          onUpdate: (self) => direction = self.direction * -1
-        }
-      })
-      
-      
-      requestAnimationFrame(animation);
+          onUpdate: (self) => (direction = self.direction * -1),
+        },
+      });
 
+      requestAnimationFrame(animation);
     },
     { scope: containerRef }
   );
 
   const animation = () => {
-    if (xPercent <= -33.33 || (direction > 0 &&  xPercent >= 33.33)) {
+    if (xPercent <= -33.33 || (direction > 0 && xPercent >= 33.33)) {
       xPercent = 0;
-    } 
+    }
 
-    gsap.set(marqueeRef.current, {xPercent});
+    gsap.set(marqueeRef.current, { xPercent });
     requestAnimationFrame(animation);
 
     xPercent += 0.02 * direction;
-  }
+  };
 
   return (
-    <div ref={containerRef} className="overflow-hidden flex justify-center pointer-events-none">
+    <div
+      ref={containerRef}
+      className="overflow-hidden flex justify-center pointer-events-none"
+    >
       <div ref={velocitySliderRef}>
         <div ref={marqueeRef} className="flex w-max">
           {children}
