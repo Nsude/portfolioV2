fd53eb38cb3b8a34b9a6249637c1a27659aa10da
Meshach Nsude <nsudeme777@gmail.com>
2025-05-08 10:10:32 +0100

    footer animations done

--- src/components/landing-page/Footer.jsx
+++ src/components/landing-page/Footer.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useNavbarContext } from '../contexts/NavbarContext';
 import { Link } from 'react-router-dom';
 import CopyIcon from '../../assets/icons/CopyIcon';
@@ -6,7 +6,9 @@ import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import useDevice from '../../components/hooks/useDevice';
 import CustomButton from '../../components/buttons/CustomButton'
-import { myEase1 } from '../utility/contansts';
+import { myEase1, myEase2 } from '../utility/contansts';
+import SlideIn from '../global/SlideIn';
+import observeElement from '../utility/customObserver';
 
 const Footer = () => {
   const {socials, copyEmail, emailCopied} = useNavbarContext();
@@ -16,6 +18,28 @@ const Footer = () => {
   const [trackCursor, setTrackCursor] = useState(false);
   const {width: deviceWidth} = useDevice();
   let revertInstance = useRef(null);
+  const bordersRef = useRef([]);
+
+  useEffect(() => {
+    const copyButton = copyButtonRef.current;
+    const borders = bordersRef.current;
+    
+    if (!(borders && copyButton)) return;
+    console.log(borders)
+    gsap.set(borders, {clipPath: "polygon(0% 0%, 0% 100%, 0% 100%, 0% 0%)"});
+
+    const animateBorders = () => {
+      gsap.to(borders, {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        duration: 1,
+        ease: myEase2,
+        stagger: .1
+      })
+    }
+
+    observeElement(copyButton, animateBorders);
+
+  }, [])
 
   const moveHandler = (e) => {
     const copyBox = copyBoxRef.current;
@@ -52,7 +76,7 @@ const Footer = () => {
       button.removeEventListener("mousemove", moveHandler);
       // make sure the revert isn't triggered over a previous revert
       if (revertInstance.current) return;
-      revertInstance.current = gsap.to(copyBox, {left: "25%", top: 0, duration: .8, delay: .2, ease: myEase1});
+      revertInstance.current = gsap.to(copyBox, {left: "25%", top: 0, duration: .8, delay: .8, ease: myEase1});
     }
 
   }, {scope: containerRef, dependencies: [trackCursor]})
@@ -72,27 +96,37 @@ const Footer = () => {
       <div className='flex flex-col lg:flex-row justify-between gap-y-[60px] lg:gap-y-[unset] mb-[60px] lg:mb-[unset]'>
       {/* "LETS BUILD" ===TEXT=== */}
         <div className='text-45-title lg:text-60-title flex flex-wrap items-center gap-x-[10px] w-[78%] md:w-[50%] xl:w-[40%] 2xl:w-[30%]'>
-          <span> Let's Build </span>
+          <SlideIn>
+            <span> Let's Build </span>
+          </SlideIn>
 
-          <span className='w-[90px] h-[50px] hidden overflow-hidden lg:inline-block rounded-4xl'> 
-            <img 
-              className='w-full h-full object-cover'
-              src="/src/assets/images/profile-image.webp" 
-              alt="small-headshot" />
-          </span>
-
-          <div className='flex items-center gap-x-[10px] lg:inline-block'>
-            <span className='w-[90px] h-[50px] overflow-hidden inline-block lg:hidden rounded-4xl'> 
+          <SlideIn>
+            <span className='w-[90px] h-[50px] hidden overflow-hidden lg:inline-block rounded-4xl'> 
               <img 
                 className='w-full h-full object-cover'
                 src="/src/assets/images/profile-image.webp" 
                 alt="small-headshot" />
             </span>
-            <span> Something </span>
-          </div>
-          <span> Unreal </span>
+          </SlideIn>
+
+          <SlideIn delay={.10}>
+            <div className='flex items-center gap-x-[10px] lg:inline-block'>
+              <span className='w-[90px] h-[50px] overflow-hidden inline-block lg:hidden rounded-4xl'> 
+                <img 
+                  className='w-full h-full object-cover'
+                  src="/src/assets/images/profile-image.webp" 
+                  alt="small-headshot" />
+              </span>
+              <span> Something </span>
+            </div>
+          </SlideIn>
+
+          <SlideIn delay={.10}>
+            <span> Unreal </span>
+          </SlideIn>
         </div>
 
+        {/* MOBILE COPY EMAIL */}
         <div className='inline-block lg:hidden'>
           <CustomButton 
             icon={<CopyIcon />}
@@ -105,19 +139,26 @@ const Footer = () => {
         {/* SOCIAL LINKS */}
         <div className='basis-[30%]' >
           <div className='flex justify-between mb-[35px] text-14-body opacity-40'>
-            <span>Socials</span>
-            <span>/04</span>
+            <SlideIn>
+              <span>Socials</span>
+            </SlideIn>
+            <SlideIn>
+              <span>/04</span>
+            </SlideIn>
           </div>
           <div className='flex justify-between text-16-body'>
             {
               socials.map((item, i) => (
-                <Link key={i} to={item.link} target='_blank'>{item.title}</Link>
+                <SlideIn key={i} delay={.05 * i}>
+                  <Link key={i} to={item.link} target='_blank'>{item.title}</Link>
+                </SlideIn>
               ))
             }
           </div>
         </div>
       </div>
 
+      {/* MOBILE BORDER-LINE */}
       <span className='w-full h-[1px] top-0 left-0 bg-myBlack opacity-25 block mb-[30px] lg:hidden' />
 
 
@@ -129,14 +170,14 @@ const Footer = () => {
         onMouseLeave={() => setTrackCursor(false)} 
         className={`relative w-full h-[90px] justify-center transition-opacity duration-[400ms] ${emailCopied ? "opacity-40" : ""} hidden lg:flex`}>
 
-        <span className='absolute w-full h-[1px] top-0 left-0 bg-myBlack opacity-25' />
+        <span ref={(el) => bordersRef.current[0] = el} className='absolute w-full h-[1px] top-0 left-0 bg-myBlack opacity-40' />
 
         <span className='flex items-center gap-x-[5px] text-25-body'>
           <span className='pt-1'> <CopyIcon size={20} /> </span>
           <span>meshachnsd@gmail.com</span>
         </span>
 
-        <span className='absolute w-full h-[1px] bottom-0 left-0 bg-myBlack opacity-25' />
+        <span ref={(el) => bordersRef.current[1] = el} className='absolute w-full h-[1px] bottom-0 left-0 bg-myBlack opacity-40' />
 
         <div ref={copyBoxRef} 
           className='h-[70px] w-[70px] bg-myWhite border-1 border-myGray rounded-[70px] flex justify-center items-center absolute top-0 translate-[-50%] left-[25%] pointer-events-none'>
@@ -146,8 +187,12 @@ const Footer = () => {
 
       {/* COPYRIGHTS STUFF */}
       <div className='w-full flex justify-between'>
-        <span>{new Date().getFullYear()}</span>
-        <span>All Rights Reserved &copy;</span>
+        <SlideIn>
+          <span>{new Date().getFullYear()}</span>
+        </SlideIn>
+        <SlideIn>
+          <span>All Rights Reserved &copy;</span>
+        </SlideIn>
       </div>
 
     </footer>
